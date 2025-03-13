6d9942ed4d50ecc56af65444a71aca8f118a5c6b
Donald <donald@hyper-cloud.kr>
2025-03-13 12:41:55 +0900

    feat: 아키텍처 설정 및 기본 파일구조

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { MainSearchScreen } from "@/screens/Home";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!</Text>
+      <Text>Open up App.tsx to start working on your app</Text>
       <StatusBar style="auto" />
     </View>
   );
