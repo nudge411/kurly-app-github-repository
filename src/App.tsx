bfb82ae507e7b20d8f92b3c4f5f9e3248fb81ff1
Donald <donald@hyper-cloud.kr>
2025-03-16 15:29:28 +0900

    feat: react-query 셋팅 및 검색결과 테스트 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "@/store";
 import { MainSearchScreen } from "@/screens/Home";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <MainSearchScreen />
+        <QueryClientProvider client={queryClient}>
+          <SafeAreaView style={{ flex: 1 }}>
+            <MainSearchScreen />
+          </SafeAreaView>
+        </QueryClientProvider>
       </PersistGate>
     </Provider>
   );
--- src/screens/Home/MainSearchScreen/Container.tsx
+++ src/screens/Home/MainSearchScreen/Container.tsx
@@ -1,43 +1,72 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useCallback, useState, useMemo } from "react";
 import { decrement, increment } from "@/store/slices/counterSlice";
 import { useAppSelector, useAppDispatch } from "@/store/hooks";
 import { githubApi } from "@/api/endpoints";
-import { useEffect } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import useDebounce from "@/hooks/useDebounde";
+import Presenter from "./Presenter";
 
 export default function Container() {
-  const count = useAppSelector((state) => state.counter.value);
-  const dispatch = useAppDispatch();
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const debouncedQuery = useDebounce(searchQuery, 500);
 
-  const test = async () => {
-    const { data, status } = await githubApi.searchRepositories({
-      q: "react-native",
-      page: 1,
-    });
-    console.log(data);
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+    error,
+    refetch,
+  } = useInfiniteQuery({
+    queryKey: ["repositories", debouncedQuery],
+    queryFn: async ({ pageParam = 1 }) => {
+      const response = await githubApi.searchRepositories({
+        q: debouncedQuery,
+        page: pageParam,
+      });
+      return response.data;
+    },
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.items.length === 30 ? allPages.length + 1 : undefined;
+    },
+    initialPageParam: 1,
+    enabled: debouncedQuery.length > 0,
+  });
+
+  const { repositories, totalCount } = useMemo(() => {
+    if (!data) return { repositories: [], totalCount: 0 };
+    return {
+      repositories: data.pages.flatMap((page) => page.items),
+      totalCount: data.pages[0]?.total_count || 0,
+    };
+  }, [data]);
+
+  const handleSearchChange = (text) => {
+    setSearchQuery(text);
   };
 
-  useEffect(() => {
-    test();
-  }, []);
+  const handleSearchSubmit = () => {
+    refetch();
+  };
+
+  const handleEndReached = useCallback(() => {
+    if (hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={() => dispatch(increment())}>
-        <Text>++</Text>
-      </TouchableOpacity>
-      <Text>{count}</Text>
-      <TouchableOpacity onPress={() => dispatch(decrement())}>
-        <Text>--</Text>
-      </TouchableOpacity>
-    </View>
+    <Presenter
+      isFetchingNextPage={isFetchingNextPage}
+      searchQuery={searchQuery}
+      handleSearchChange={handleSearchChange}
+      handleSearchSubmit={handleSearchSubmit}
+      totalCount={totalCount}
+      error={error}
+      repositories={repositories}
+      handleEndReached={handleEndReached}
+      status={status}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
